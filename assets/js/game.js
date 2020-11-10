b4370446af03549c4922508ca6bc11147945a30e
zanderson1998 <zanderson1998@yahoo.com>
2020-11-09 20:14:52 -0700

    Add name input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log(playerName + " is ready for battle!");
+// this creates a function named fight
+function fight(){
+    window.alert("The fight has begun!");
+}
+
+//fight();
