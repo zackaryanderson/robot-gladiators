d11dfc52eefd6706b44e0bae40ed54936689f848
zanderson1998 <zanderson1998@yahoo.com>
2020-11-12 16:41:07 -0700

    fix empty player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,16 @@
+var getPlayerName = function(){
+    var name = "";
+
+    while (name === "" || name === null){
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robots name"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
